62c55e7b2fa418475d69bf2dcbe33c75cc743569
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-01 13:47:23 +0000

    Takes user inputer and check if valid

--- mastermind.rb
+++ mastermind.rb
@@ -1,4 +1,6 @@
 class Mastermind
+  COLORS = ["r", "g", "b", "y", "o", "p"]
+
   def initialize
     @computer_choices = []
     user_choices = []
@@ -7,8 +9,6 @@ class Mastermind
     total_rounds = 0
   end
 
-  COLORS = ["red", "green", "blue", "yellow", "orange", "purple"]
-
   def computer_random_choice
     4.times do
       @computer_choices.push(COLORS[rand(0...5)])
@@ -16,8 +16,19 @@ class Mastermind
     puts @computer_choices
   end
 
+  def user_guesses
+    puts "You can choose four following six colours: Red, Green, Blue, Yellow, Orange, or Purple"
+    puts "Please enter your selection in the following format [RGBY] without the square brackets"
+    user_input = gets.strip.downcase
+    user_choices = user_input.split("")
+  end
+
+  def valid?
+    if user_choices.length == 4 and (user_choices - COLORS).empty?
+    end
+  end
+
 end
 
 game = Mastermind.new
 
-game.computer_random_choice
