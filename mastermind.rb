d4d94a2fe0b1a39d81a58c2d38f98c7c68a3e914
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-01 13:15:08 +0000

    Computer generate 4 different colours

---
+++ mastermind.rb
@@ -0,0 +1,23 @@
+class Mastermind
+  def initialize
+    $computer_choices = []
+    user_choices = []
+    correct_count = 0
+    correct_color_count = 0
+    total_rounds = 0
+  end
+
+  COLORS = ["red", "green", "blue", "yellow", "orange", "purple"]
+
+  def computer_random_choice
+    4.times do
+      $computer_choices.push(COLORS[rand(0...5)])
+    end
+    puts $computer_choices
+  end
+
+end
+
+game = Mastermind.new
+
+game.computer_random_choice
