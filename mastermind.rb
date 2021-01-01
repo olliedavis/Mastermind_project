262aec6fa0b8f104b99ec74b76da1ff629305828
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-01-01 13:22:30 +0000

    Syntax correction

--- mastermind.rb
+++ mastermind.rb
@@ -1,6 +1,6 @@
 class Mastermind
   def initialize
-    $computer_choices = []
+    @computer_choices = []
     user_choices = []
     correct_count = 0
     correct_color_count = 0
@@ -11,9 +11,9 @@ class Mastermind
 
   def computer_random_choice
     4.times do
-      $computer_choices.push(COLORS[rand(0...5)])
+      @computer_choices.push(COLORS[rand(0...5)])
     end
-    puts $computer_choices
+    puts @computer_choices
   end
 
 end
